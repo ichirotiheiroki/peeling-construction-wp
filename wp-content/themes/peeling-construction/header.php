83f18329201f6791d4bb411bdc9a3c19fe22e0ec
ichirotiheiroki <deadichi@gmail.com>
2025-03-19 02:23:35 +0400

    logo

--- wp-content/themes/peeling-construction/header.php
+++ wp-content/themes/peeling-construction/header.php
@@ -80,17 +80,16 @@
 
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-0 pe-5">
-    <a href="index.html" class="navbar-brand ps-5 me-0">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="">
+    <a href="/#" class="navbar-brand ps-5 me-0">
+        <img class="navbar-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="">
     </a>
     <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="index.html" class="nav-item nav-link active">Home</a>
-            <a href="about.html" class="nav-item nav-link">About</a>
-            <a href="service.html" class="nav-item nav-link">Services</a>
+            <a href="#about" class="nav-item nav-link">О нас</a>
+            <a href="#service" class="nav-item nav-link">Решения</a>
 <!--            <div class="nav-item dropdown">-->
 <!--                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>-->
 <!--                <div class="dropdown-menu bg-light m-0">-->
@@ -101,7 +100,7 @@
 <!--                    <a href="404.html" class="dropdown-item">404 Page</a>-->
 <!--                </div>-->
 <!--            </div>-->
-            <a href="contact.html" class="nav-item nav-link">Contact</a>
+            <a href="#contact" class="nav-item nav-link">Контакты</a>
         </div>
         <a href="" class="btn btn-primary px-3 d-none d-lg-block">Get A Quote</a>
     </div>
