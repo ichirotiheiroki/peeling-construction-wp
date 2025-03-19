df9063e203be9443856bb91947e06090f40acc97
ichirotiheiroki <deadichi@gmail.com>
2025-03-19 10:18:03 +0400

    about

--- wp-content/themes/peeling-construction/js/main.js
+++ wp-content/themes/peeling-construction/js/main.js
@@ -101,17 +101,21 @@
         dots: false,
         loop: true,
         nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
+
         responsive: {
             0:{
                 items:1
             },
             768:{
                 items:2
+            },
+            992:{
+                items:5
+            },
+            1200:{
+                items:6
             }
+
         }
     });
 
