30e0e16ad9d8497a339342866b029b13834040ee
ichirotiheiroki <deadichi@gmail.com>
2025-03-19 21:03:59 +0400

    about

--- wp-content/themes/peeling-construction/landing.php
+++ wp-content/themes/peeling-construction/landing.php
@@ -315,7 +315,7 @@ get_header();
     <!-- Project Start -->
     <div class="container-fluid bg-dark pt-5 my-5 px-0">
         <div class="text-center mx-auto mt-5 wow fadeIn" data-wow-delay="0.1s" style="max-width: 600px;">
-            <p class="fw-medium text-uppercase text-primary mb-2">Наши проекты</p>
+            <p class="fw-medium text-uppercase text-white mb-2">Наши проекты</p>
             <h1 class="display-5 text-white mb-5">Проекты выполненные нами</h1>
         </div>
         <div class="owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
