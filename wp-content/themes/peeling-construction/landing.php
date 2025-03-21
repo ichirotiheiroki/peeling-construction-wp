18bfea402cb73e778b530bfb98d115bc6761b5c3
ziyamahmudov <deadichi@gmail.com>
2025-03-21 12:26:07 +0400

    ui fix

--- wp-content/themes/peeling-construction/landing.php
+++ wp-content/themes/peeling-construction/landing.php
@@ -316,7 +316,7 @@ get_header();
     <div class="container-fluid bg-dark pt-5 my-5 px-0">
         <div class="text-center mx-auto mt-5 wow fadeIn" data-wow-delay="0.1s" style="max-width: 600px;">
             <p class="fw-medium text-uppercase text-white mb-2">Наши проекты</p>
-            <h1 class="display-5 text-white mb-5">Проекты выполненные нами</h1>
+            <h1 class="display-5 text-white mb-5">Проекты выполненные нашей компанией</h1>
         </div>
         <div class="owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
             <a class="project-item"  >
