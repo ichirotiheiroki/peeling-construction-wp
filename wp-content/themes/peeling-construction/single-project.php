d03c965533483f76730431242b4e904dc267d255
ichirotiheiroki <deadichi@gmail.com>
2025-03-31 13:19:33 +0400

    projects

--- wp-content/themes/peeling-construction/single-project.php
+++ wp-content/themes/peeling-construction/single-project.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-    <div class="container-fluid bg-dark pt-5 my-5 px-0">
+    <div class="container-fluid     pt-5 my-5 px-0">
         <h1 class="project-post-title"><?php the_title(); ?></h1>
         <div class="project-post-content"><?php the_content(); ?></div>
     </div>
