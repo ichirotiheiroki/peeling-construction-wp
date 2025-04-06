995ab166458662e183ad717525bf5530834b9a5d
ichirotiheiroki <deadichi@gmail.com>
2025-04-07 01:04:56 +0400

    Projects

--- wp-content/themes/peeling-construction/project-card.php
+++ wp-content/themes/peeling-construction/project-card.php
@@ -1,8 +1,8 @@
-<a href="<?php echo get_permalink(); ?>" class="col-md-6 col-lg-4 wow fadeInUp"
-   data-wow-delay="0.1s">
+<a href="<?php echo get_permalink(); ?>" class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
     <div class="service-item">
         <?php
         $thumbnail_id = get_the_ID();
+
         $thumbnail_url = get_the_post_thumbnail_url($thumbnail_id, 'full');
 
         if (!$thumbnail_url) {
@@ -11,9 +11,19 @@
 
         echo '<img src="' . esc_url($thumbnail_url) . '" class="img-fluid ratio ratio-1x1 object-fit-cover" alt="' . esc_attr(get_the_title()) . '">';
         ?>
+
         <div class="service-img">
-            <?php echo get_the_post_thumbnail(get_the_ID(), 'thumbnail', ['class' => 'img-fluid']); ?>
+            <?php
+            $second_thumbnail_url = get_the_post_thumbnail_url($thumbnail_id, 'thumbnail');
+
+            if (!$second_thumbnail_url) {
+                $second_thumbnail_url = get_template_directory_uri() . '/img/projects-placeholder.jpg';
+            }
+
+            echo '<img src="' . esc_url($second_thumbnail_url) . '" class="img-fluid" alt="' . esc_attr(get_the_title()) . '">';
+            ?>
         </div>
+
         <div class="service-detail">
             <div class="service-title">
                 <hr class="w-25">
@@ -24,6 +34,5 @@
                 <p class="text-white mb-0"><?php the_excerpt(); ?></p>
             </div>
         </div>
-        <!--                        <a class="btn btn-light" href="">Read More</a>-->
     </div>
 </a>
