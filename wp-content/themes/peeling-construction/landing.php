eeb4e89db0c0efd775102ad753198dbf4cc73bfc
ichirotiheiroki <deadichi@gmail.com>
2025-04-07 00:58:48 +0400

    Projects

--- wp-content/themes/peeling-construction/landing.php
+++ wp-content/themes/peeling-construction/landing.php
@@ -532,40 +532,11 @@ get_header();
 
                 foreach ($my_posts as $post) {
                     setup_postdata($post);
-                    ?>
 
 
-                    <a href="<?php echo get_permalink(); ?>" class="col-md-6 col-lg-4 wow fadeInUp"
-                       data-wow-delay="0.1s">
-                        <div class="service-item">
-                            <?php
-                            $thumbnail_id = get_the_ID();
-                            $thumbnail_url = get_the_post_thumbnail_url($thumbnail_id, 'full');
 
-                            if (!$thumbnail_url) {
-                                $thumbnail_url = get_template_directory_uri() . '/img/projects-placeholder.jpg';
-                            }
+                    get_template_part('content', 'project-card.php');
 
-                            echo '<img src="' . esc_url($thumbnail_url) . '" class="img-fluid ratio ratio-1x1 object-fit-cover" alt="' . esc_attr(get_the_title()) . '">';
-                            ?>
-                            <div class="service-img">
-                                <?php echo get_the_post_thumbnail(get_the_ID(), 'thumbnail', ['class' => 'img-fluid']); ?>
-                            </div>
-                            <div class="service-detail">
-                                <div class="service-title">
-                                    <hr class="w-25">
-                                    <h3 class="mb-0"><?php the_title(); ?></h3>
-                                    <hr class="w-25">
-                                </div>
-                                <div class="service-text text-white">
-                                    <p class="text-white mb-0"><?php the_excerpt(); ?></p>
-                                </div>
-                            </div>
-                            <!--                        <a class="btn btn-light" href="">Read More</a>-->
-                        </div>
-                    </a>
-
-                    <?php
 
 
                 }
--- wp-content/themes/peeling-construction/projects.php
+++ wp-content/themes/peeling-construction/projects.php
@@ -33,32 +33,8 @@ get_header();
 
                 foreach( $my_posts as $post ){
                     setup_postdata( $post );
-                    ?>
 
-
-                    <a href="<?php echo get_permalink(); ?>" class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="service-item">
-                            <?php echo get_the_post_thumbnail(get_the_ID(), 'full', ['class' => 'img-fluid ratio ratio-1x1 object-fit-cover']); ?>
-                            <div class="service-img">
-                                <?php echo get_the_post_thumbnail(get_the_ID(), 'thumbnail', ['class' => 'img-fluid']); ?>
-                            </div>
-                            <div class="service-detail">
-                                <div class="service-title">
-                                    <hr class="w-25">
-                                    <h3 class="mb-0"><?php the_title(); ?></h3>
-                                    <hr class="w-25">
-                                </div>
-                                <div class="service-text text-white">
-                                    <p class="text-white mb-0"><?php the_excerpt(); ?></p>
-                                </div>
-                            </div>
-                            <!--                        <a class="btn btn-light" href="">Read More</a>-->
-                        </div>
-                    </a>
-
-                    <?php
-
-
+                    get_template_part('content', 'project-card.php');
 
                 }
 
